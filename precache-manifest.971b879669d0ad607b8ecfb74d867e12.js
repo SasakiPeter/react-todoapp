81c2f1e63ea2bac87e2b41baaa2a15181d3be12e
SasakiPeter <anagentofthegod@gmail.com>
2018-11-18 23:48:51 +0900

    Updates

---
+++ precache-manifest.971b879669d0ad607b8ecfb74d867e12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1f04d793cf16cdaa992",
+    "url": "/react-todoapp/static/js/runtime~main.b1f04d79.js"
+  },
+  {
+    "revision": "c1bc4be49a3b6c963597",
+    "url": "/react-todoapp/static/js/main.c1bc4be4.chunk.js"
+  },
+  {
+    "revision": "583f172dc76b61d58792",
+    "url": "/react-todoapp/static/js/1.583f172d.chunk.js"
+  },
+  {
+    "revision": "c1bc4be49a3b6c963597",
+    "url": "/react-todoapp/static/css/main.4c1d5d8a.chunk.css"
+  },
+  {
+    "revision": "2057b40df969b94e379e30eaf1062d3d",
+    "url": "/react-todoapp/index.html"
+  }
+];
